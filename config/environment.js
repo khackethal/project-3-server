aeb66add8d7ba59c48f24863dc12e36f4d638f03
kath <khackethal.freelance@gmail.com>
2021-05-21 13:58:03 +0100

    start on file structure

---
+++ config/environment.js
@@ -0,0 +1,8 @@
+// ! database ULR- we need to add
+export const dbURL = 'mongodb://localhost/ 
+
+export const port = 4000
+
+// ? A Secret string for JWT - random long words strung together are hardest to guess for computers
+//! Let's add a random word
+export const secret =
