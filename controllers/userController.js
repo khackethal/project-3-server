abacc29164e3012131d7cc599fef85617bd5dafb
kath <khackethal.freelance@gmail.com>
2021-06-01 13:01:48 +0100

    getting changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,19 +3,29 @@ import { NotUnique, NotValid } from '../lib/errors.js'
 import jwt from 'jsonwebtoken'
 import { secret } from '../config/environment.js'
 
-
 async function register(req, res, next) {
   try {
-    const userMail = await User.findOne({ email: req.body.email })
-    const userName = await User.findOne({ username: req.body.username })
-  
 
-    if (userMail || userName) {
+    const userFound = await User.findOne(
+      { $or: [
+        { username: req.body.username },
+        { email: req.body.email }
+      ] } )
+
+    if (userFound) {
       throw new NotUnique
-    } else if ((!userMail) && (!userName)) {
-      const newUser = await User.create(req.body)
-      res.status(200).json({ message: `Sign up successful, welcome ${newUser.username} !` })
     }
+    
+    const newUser = await User.create(req.body)
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      secret,
+      { expiresIn: '12h' }
+    )
+
+    res.status(200).json({ message: `Sign up successful, welcome ${newUser.username} !`, token })
+
   } catch (err) {
     next(err)
   }
@@ -23,6 +33,7 @@ async function register(req, res, next) {
 
 async function login(req, res, next) {
   try {
+
     const user = await User.findOne({ email: req.body.email })
 
     if (!user) {
@@ -41,7 +52,6 @@ async function login(req, res, next) {
       { expiresIn: '12h' }
     )
 
-    console.log('Successful sign in')
     res.status(200).json({ message: `Login successful, welcome back ${user.username}!`, token })
     console.log(user)
 
@@ -50,8 +60,26 @@ async function login(req, res, next) {
   }
 }
 
+async function checkUnique(req,res,next) {
+  try {
+
+    const user = await User.findOne({ username: req.body.username })
+    const email = await User.findOne({ email: req.body.email })
+
+    if (!user && !email) {
+      res.sendStatus(200)
+    } else {
+      throw new NotUnique
+    }
+
+  } catch (err) {
+    console.log('err.name: ', err.name)
+    next(err)
+  }
+}
 
 export default {
   register,
   login,
+  checkUnique,
 }
